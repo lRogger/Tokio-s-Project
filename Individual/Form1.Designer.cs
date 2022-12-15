316abea9fca582ed3575a4832b2a8b00586db8a1
Rogger Parraga <roggerp98@gmail.com>
2022-12-15 10:42:18 -0500

    Cambio1

--- Individual/Form1.Designer.cs
+++ Individual/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,11 +37,9 @@
             this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
-            this.utilidadesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.prueba = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.utilidadesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.prueba)).BeginInit();
             this.SuspendLayout();
             // 
@@ -107,17 +104,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(115, 51);
+            this.button1.Location = new System.Drawing.Point(16, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 72);
+            this.button1.Size = new System.Drawing.Size(116, 72);
             this.button1.TabIndex = 3;
             this.button1.Text = "Users";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // utilidadesBindingSource
-            // 
-            this.utilidadesBindingSource.DataSource = typeof(LibreriaGrupal.Utilidades);
             // 
             // prueba
             // 
@@ -137,7 +130,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(395, 51);
+            this.button2.Location = new System.Drawing.Point(173, 51);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 72);
             this.button2.TabIndex = 5;
@@ -162,7 +155,6 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.utilidadesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.prueba)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -179,7 +171,6 @@
         private ToolStripMenuItem editarToolStripMenuItem;
         private ToolStripMenuItem verToolStripMenuItem;
         private Button button1;
-        private BindingSource utilidadesBindingSource;
         private DataGridView prueba;
         private Button button2;
     }
