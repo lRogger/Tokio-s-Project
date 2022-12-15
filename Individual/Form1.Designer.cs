cd2188ac76a918bea02b5e3e8726a7778b6911ff
Rogger Parraga <roggerp98@gmail.com>
2022-12-14 19:01:23 -0500

    Agregar archivos de proyecto.

---
+++ Individual/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Individual
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.utilidadesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.prueba = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.utilidadesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prueba)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.verToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.cerrarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // verToolStripMenuItem
+            // 
+            this.verToolStripMenuItem.Name = "verToolStripMenuItem";
+            this.verToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.verToolStripMenuItem.Text = "Ver";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Users";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // utilidadesBindingSource
+            // 
+            this.utilidadesBindingSource.DataSource = typeof(LibreriaGrupal.Utilidades);
+            // 
+            // prueba
+            // 
+            this.prueba.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.prueba.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.prueba.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.prueba.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.prueba.Location = new System.Drawing.Point(16, 139);
+            this.prueba.Name = "prueba";
+            this.prueba.RowTemplate.Height = 25;
+            this.prueba.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.prueba.Size = new System.Drawing.Size(604, 150);
+            this.prueba.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(395, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 72);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "comida xd";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(632, 301);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.prueba);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.utilidadesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prueba)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem cerrarToolStripMenuItem;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem verToolStripMenuItem;
+        private Button button1;
+        private BindingSource utilidadesBindingSource;
+        private DataGridView prueba;
+        private Button button2;
+    }
+}
---
+++ LibreriaGrupal/LibreriaGrupal.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace LibreriaGrupal
+{
+    public class Utilidades
+    {
+
+        public static DataSet consultar (string cmd)
+        {
+            string server = "localhost";
+            string user = "root";
+            string pass ="";
+            string puerto = "3306";
+            string db = "chichar";
+            string strCon;
+
+
+            DataSet ds = new DataSet();
+            strCon = "server=" + server + ";port=" + puerto + 
+                ";user id=" + user + ";password=" + pass + ";database=" + db + ";";
+            MySqlConnection con = new MySqlConnection(strCon);
+            try
+            {
+
+                
+                con.ConnectionString = strCon;
+                con.Open();
+
+                MySqlDataAdapter dp = new MySqlDataAdapter(cmd, con);
+
+                dp.Fill(ds);
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: "+e);
+            }
+
+            return ds;
+            
+        }
+
+        public bool validar(char e, string tipo)
+        {
+            switch (tipo)
+            {
+                case "numero":
+                    if (!char.IsNumber(e) && !char.IsControl(e))
+                    {
+                        return true;
+                    }
+                    break;
+
+                case "letras":
+                    if(!char.IsLetter(e) && !char.IsControl(e))
+                    {
+                        return true;
+                    }
+                    break;
+                    
+            }
+            return false;
+        }
+
+    }
+}//Lindsay estuvo aqui
