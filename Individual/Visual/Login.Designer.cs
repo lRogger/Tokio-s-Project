90edcc57c70ac8d334d3ffb497eb797ef384894e
Rogger Parraga <roggerp98@gmail.com>
2022-12-16 00:15:38 -0500

    Más diseño

--- Individual/Visual/Login.Designer.cs
+++ Individual/Visual/Login.Designer.cs
@@ -32,12 +32,12 @@
             this.pwd = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label2 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.user = new System.Windows.Forms.TextBox();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.cerrar = new System.Windows.Forms.Label();
@@ -46,17 +46,20 @@
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pwd
             // 
-            this.pwd.Location = new System.Drawing.Point(39, 217);
+            this.pwd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pwd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pwd.Location = new System.Drawing.Point(39, 201);
             this.pwd.Name = "pwd";
             this.pwd.PasswordChar = '●';
-            this.pwd.Size = new System.Drawing.Size(261, 23);
-            this.pwd.TabIndex = 1;
+            this.pwd.PlaceholderText = "Contraseña";
+            this.pwd.Size = new System.Drawing.Size(261, 18);
+            this.pwd.TabIndex = 2;
             this.pwd.Enter += new System.EventHandler(this.pwd_Enter);
             this.pwd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pwd_KeyPress);
             // 
@@ -68,7 +71,7 @@
             this.button1.Location = new System.Drawing.Point(39, 292);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(261, 31);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Iniciar Sesión";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -77,6 +80,7 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Purple;
             this.linkLabel1.Location = new System.Drawing.Point(199, 254);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(92, 12);
@@ -84,18 +88,6 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Olvidé mi contraseña";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(37, 198);
-            this.label2.Margin = new System.Windows.Forms.Padding(0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Contraseña:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // checkBox1
             // 
@@ -106,33 +98,22 @@
             this.checkBox1.Location = new System.Drawing.Point(39, 251);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(81, 16);
-            this.checkBox1.TabIndex = 3;
+            this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "Recordar usuario";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(37, 140);
-            this.label1.Margin = new System.Windows.Forms.Padding(0);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
-            this.label1.Size = new System.Drawing.Size(63, 19);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Cédula:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
             // user
             // 
             this.user.BackColor = System.Drawing.Color.White;
-            this.user.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.user.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.user.CausesValidation = false;
             this.user.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.user.Location = new System.Drawing.Point(39, 162);
+            this.user.Location = new System.Drawing.Point(39, 161);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(261, 22);
-            this.user.TabIndex = 0;
+            this.user.PlaceholderText = "Cédula";
+            this.user.Size = new System.Drawing.Size(261, 15);
+            this.user.TabIndex = 1;
             this.user.TextChanged += new System.EventHandler(this.user_TextChanged);
             this.user.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.user_KeyPress);
             // 
@@ -144,16 +125,15 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.Window;
-            this.splitContainer1.Panel1.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel3);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.splitter1);
             this.splitContainer1.Panel1.Controls.Add(this.linkLabel1);
             this.splitContainer1.Panel1.Controls.Add(this.user);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
             this.splitContainer1.Panel1.Controls.Add(this.pwd);
             this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
-            this.splitContainer1.Panel1.Controls.Add(this.label2);
             this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
             // 
             // splitContainer1.Panel2
@@ -167,25 +147,43 @@
             this.splitContainer1.TabIndex = 5;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
             // 
-            // pictureBox2
+            // panel3
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(37, 89);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(27)))), ((int)(((byte)(141)))));
+            this.panel3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel3.Location = new System.Drawing.Point(39, 225);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 2);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(27)))), ((int)(((byte)(141)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(39, 182);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 2);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(27)))), ((int)(((byte)(141)))));
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 2);
+            this.panel2.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(27)))), ((int)(((byte)(141)))));
-            this.label3.Location = new System.Drawing.Point(72, 84);
+            this.label3.Location = new System.Drawing.Point(33, 82);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(228, 41);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Inicia Sesion";
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Iniciar Sesion";
             this.label3.Click += new System.EventHandler(this.label3_Click_1);
             // 
             // splitter1
@@ -202,8 +200,8 @@
             this.cerrar.BackColor = System.Drawing.Color.Transparent;
             this.cerrar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cerrar.ForeColor = System.Drawing.Color.Purple;
-            this.cerrar.Location = new System.Drawing.Point(352, 8);
-            this.cerrar.Margin = new System.Windows.Forms.Padding(5);
+            this.cerrar.Location = new System.Drawing.Point(352, 0);
+            this.cerrar.Margin = new System.Windows.Forms.Padding(0);
             this.cerrar.Name = "cerrar";
             this.cerrar.Padding = new System.Windows.Forms.Padding(5);
             this.cerrar.Size = new System.Drawing.Size(40, 42);
@@ -240,7 +238,7 @@
             this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -251,15 +249,15 @@
         private TextBox pwd;
         private Button button1;
         private LinkLabel linkLabel1;
-        private Label label2;
         private CheckBox checkBox1;
-        private Label label1;
         private TextBox user;
         private SplitContainer splitContainer1;
         private Splitter splitter1;
         private Label label3;
         private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
         private Label cerrar;
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
     }
 }
