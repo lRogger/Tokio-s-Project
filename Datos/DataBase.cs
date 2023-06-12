7c9b787371decea756ae803ee713dcc080a7e193
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-06-12 18:30:06 -0500

    CAMBIADA CADENA DE CONEXION

--- Datos/DataBase.cs
+++ Datos/DataBase.cs
@@ -15,11 +15,11 @@ namespace Datos
         public DataBase()
         {
             
-            //SE EL SERVIDOR DE LA BASE DE DATOS
-            this.server = "tokio.database.windows.net";
+            //SE CAMBIO EL SERVIDOR DE LA BASE DE DATOS
+            this.server = "tokio-server.database.windows.net";
             this.user = "tokio";
             this.pass = "semestre6!";
-            this.db = "TOKIO_PROYECTO";
+            this.db = "TOKIO-PROYECTO";
 
             this.strCon = $"Data Source={server};Initial Catalog={db};Persist Security Info=True;User ID={user};Password={pass};";
 
